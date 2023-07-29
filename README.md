2be97d3366b3ae3f84d6a9b100fc951dfc34ea83
andresTapa <79794569+andresTapari@users.noreply.github.com>
2023-07-29 13:41:53 -0300

    Screenshot added

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-
+<p><center>
 
 [![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
 [![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
 [![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
 
+</center></p>
 
-# TOP DOWN SPACE SHOOTER ROUGE LIKE 
+# Video Game: HAHAi 
 
 Top down shootr rouge like, where you control a space ship
 
+## ScreenShots
+<img src="assets/screenshots/20230729-133801.png" width = auto style="image-rendering: pixelated;"> </img>
+<img src="assets/screenshots/20230729-133617.png" width = auto style="image-rendering: pixelated;"> </img>
+<img src="assets\screenshots\20230729-133642.png" width = auto style="image-rendering: pixelated;"> </img>
 
-## Demo
+## Play now!
+Link pendiente
 
-Insert gif or link to demo
-
+## 
---
+++ scripts/GLOBALS/screenshots.gd
@@ -0,0 +1,21 @@
+extends Node
+
+func _input(event):
+	# Verificar si se presionó la tecla "ImpPnt" (Imprimir Pantalla)
+	if event is InputEventKey and event.keycode == KEY_SPACE:
+		# Obtener el viewport actual
+#		var viewport = get_viewport()
+
+		# Crear una textura para capturar el viewport
+#		var texture = ImageTexture.new()
+#		texture.create_from_image(viewport.get_texture().get_image())
+
+		# Obtener la fecha y hora actual para usarlo en el nombre del archivo
+		var date_time 		= Time.get_date_dict_from_system()
+		var date_time_clock = Time.get_time_dict_from_system()
+		var timestamp = str(date_time.year) + str(date_time.month).pad_zeros(2) + str(date_time.day).pad_zeros(2) + "-" + str(date_time_clock.hour).pad_zeros(2) + str(date_time_clock.minute).pad_zeros(2) + str(date_time_clock.second).pad_zeros(2)
+
+		# Guardar la textura en un archivo PNG en la carpeta del proyecto
+		var file_path = "res://assets/screenshots/" + timestamp + ".png"
+		get_viewport().get_texture().get_image().save_png(file_path)
+		print("Captura de pantalla guardada como: " + file_path)
