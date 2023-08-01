7333e8cf7ba8b2297dc88b4867373d5478b4ff1c
andresTapa <79794569+andresTapari@users.noreply.github.com>
2023-08-01 13:14:54 -0300

    Sound Setup

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -4,10 +4,6 @@ func _ready():
 	# Reiniciamos puntaje
 	Score.set("score",0)
 
-func _on_setting_btn_pressed():
-#	get_tree().change_scene_to_file("res://scenes/lvl/dev_lvl.tscn")
-	pass
-
 # Esta función se ejecuta cuando se presiona el boton PLAY, inicia el juego 
 func _on_play_btn_pressed():
-	get_tree().change_scene_to_file("res://scenes/lvl/dev_lvl.tscn")
+	get_tree().change_scene_to_file("res://scenes/lvl/levels/tutorial_level.tscn")
